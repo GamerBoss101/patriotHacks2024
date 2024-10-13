c278166e09c2cd3a14aa10c91293bee2479c16d2
Ryan Zhang <ryanz22@vt.edu>
2024-10-13 03:54:26 -0400

    Logo & Icon

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,6 +11,7 @@ import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/link";
 import NextLink from "next/link";
 import { usePathname } from 'next/navigation';
+import Image from "next/image";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -27,7 +28,15 @@ export const Navbar = () => {
         <NextUINavbar maxWidth="xl" position="sticky">
             <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
                 <NavbarBrand as="li" className="gap-3">
-                    <NextLink className="flex justify-start items-center gap-1" href="/">
+                    <NextLink className="flex justify-start items-center" href="/">
+                        <Image
+                            alt="Carbin Logo"
+                            className="h-8 w-8"
+                            height={48}
+                            src="/carbin.png"
+                            width={48}
+                        />
+
                         <p className="text-2xl font-bold font-baskerville">Carbin</p>
                     </NextLink>
                 </NavbarBrand>
